8bb99c0400d136b03f84ea61df73421d60ceb24e
Weizhe Gao <52844604+Quixot1k@users.noreply.github.com>
2023-08-05 16:27:06 -0700

    use forwardRef to expose BottomSheet

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import BottomSheet from "./components/BottomSheet";
+import { useRef } from "react";
 
 export default function App() {
+  const bottomSheetRef = useRef(null);
+  const onPress = () => {
+    bottomSheetRef.current?.scrollTo(-150);
+  };
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
         <StatusBar style="auto" />
-        <BottomSheet />
+        <TouchableOpacity style={styles.button} onPress={onPress} />
+        <BottomSheet ref={bottomSheetRef}>
+          <View style={{ flex: 1, backgroundColor: "#f5f5f5" }} />
+        </BottomSheet>
       </View>
     </GestureHandlerRootView>
   );
@@ -20,5 +28,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#151515",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    height: 50,
+    borderRadius: 25,
+    aspectRatio: 1,
+    backgroundColor: "white",
+    opacity: 0.6,
   },
 });
--- components/BottomSheet.jsx
+++ components/BottomSheet.jsx
@@ -1,71 +1,95 @@
-import { StyleSheet } from 'react-native'
-import React, { useCallback, useEffect } from 'react'
+import { View, StyleSheet } from 'react-native'
+import React, { forwardRef, useCallback, useEffect, useImperativeHandle } from 'react'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import Animated, { Extrapolate, interpolate, useAnimatedStyle, useScrollViewOffset, useSharedValue, withSequence, withSpring, withTiming } from 'react-native-reanimated';
+import Animated, { Extrapolate, interpolate, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 import DIMENSION from '../constants/DIMENSION'
 
+/* constants */
 const { SCREEN_WIDTH, SCREEN_HEIGHT } = DIMENSION;
 const MAX_TRANS_Y = -SCREEN_HEIGHT + 70
 const MIN_TRANS_Y = -SCREEN_HEIGHT / 3
 
-const BottomSheet = () => {
-    const bottomSheetTransY = useSharedValue(0);
-    const scrollTo = useCallback((destination) => {
-        'worklet';
-        bottomSheetTransY.value = withSpring(destination, { damping: 17.5 });
-    }, []);
-    const bottomSheetContext = useSharedValue(0);
-    const gesture = Gesture.Pan()
-        .onStart(() => {
-            bottomSheetContext.value = bottomSheetTransY.value;
-        })
-        .onUpdate(e => {
-            bottomSheetTransY.value = e.translationY + bottomSheetContext.value;
-            bottomSheetTransY.value = Math.max(-SCREEN_HEIGHT, bottomSheetTransY.value);
-        })
-        .onEnd(() => {
-            if (bottomSheetTransY.value > MIN_TRANS_Y) {
-                scrollTo(MIN_TRANS_Y)
-            } else if (bottomSheetTransY.value < MIN_TRANS_Y || bottomSheetTransY.value < MAX_TRANS_Y) {
-                scrollTo(MAX_TRANS_Y)
+const BottomSheet = forwardRef(
+    ({ children }, ref) => {
+        /* shared values */
+        const bottomSheetTransY = useSharedValue(0);
+        const bottomSheetContext = useSharedValue(0);
+
+        /* callbacks */
+        const scrollTo = useCallback((destination) => {
+            'worklet';
+            bottomSheetTransY.value = withSpring(destination, { damping: 17.5 });
+        }, []);
+        useImperativeHandle(ref,
+            () => ({
+                // methods
+                scrollTo,
+            }),
+            // deps
+            [scrollTo])
+
+        /* gestures */
+        const gesture = Gesture.Pan()
+            .onStart(() => {
+                // automatically drag to MIN_TRANS_Y at beginning
+                bottomSheetContext.value = bottomSheetTransY.value;
+            })
+            .onUpdate(e => {
+                // e.translationY: mouse position change relative to the starting position
+                // bottomSheetContext.value: bottomSheet start position
+                // bottomSheetTransY.value: bottomSheet current position
+                bottomSheetTransY.value = e.translationY + bottomSheetContext.value;
+                bottomSheetTransY.value = Math.max(-SCREEN_HEIGHT, bottomSheetTransY.value);
+            })
+            .onEnd(() => {
+                if (bottomSheetTransY.value > MIN_TRANS_Y) {
+                    scrollTo(MIN_TRANS_Y)
+                } else if (bottomSheetTransY.value < MIN_TRANS_Y || bottomSheetTransY.value < MAX_TRANS_Y) {
+                    scrollTo(MAX_TRANS_Y)
+                }
+            });
+
+        /* styles */
+        const rBottomSheetStyle = useAnimatedStyle(() => {
+            const borderRadius = interpolate(
+                bottomSheetTransY.value,
+                [MAX_TRANS_Y / 2, MAX_TRANS_Y],
+                [20, 40],
+                Extrapolate.CLAMP
+            );
+            return {
+                borderRadius: borderRadius,
+                transform: [
+                    { translateY: bottomSheetTransY.value }
+                ]
             }
-        });
-    const rBottomSheetStyle = useAnimatedStyle(() => {
-        const borderRadius = interpolate(
-            bottomSheetTransY.value,
-            [MAX_TRANS_Y / 2, MAX_TRANS_Y],
-            [20, 40],
-            Extrapolate.CLAMP
-        );
-        return {
-            borderRadius: borderRadius,
-            transform: [
-                { translateY: bottomSheetTransY.value }
-            ]
-        }
-    })
-    const rHandleBarStyle = useAnimatedStyle(() => {
-        const width = interpolate(
-            bottomSheetTransY.value,
-            [-SCREEN_HEIGHT, 0],
-            [0.125 * SCREEN_WIDTH, 0.20 * SCREEN_WIDTH],
-            Extrapolate.CLAMP
+        })
+        const rHandleBarStyle = useAnimatedStyle(() => {
+            const width = interpolate(
+                bottomSheetTransY.value,
+                [-SCREEN_HEIGHT, 0],
+                [0.15 * SCREEN_WIDTH, 0.225 * SCREEN_WIDTH],
+                Extrapolate.CLAMP
+            )
+            return {
+                width: width,
+            }
+        })
+
+        /* render */
+        useEffect(() => {
+            scrollTo(MIN_TRANS_Y)
+        }, [])
+        return (
+            <GestureDetector gesture={gesture}>
+                <Animated.View style={[styles.bottomSheetContainer, rBottomSheetStyle]}>
+                    <Animated.View style={[styles.handleBar, rHandleBarStyle]} />
+                    {children}
+                </Animated.View>
+            </GestureDetector>
         )
-        return {
-            width: width,
-        }
-    })
-    useEffect(() => {
-        scrollTo(MIN_TRANS_Y)
-    }, [])
-    return (
-        <GestureDetector gesture={gesture}>
-            <Animated.View style={[styles.bottomSheetContainer, rBottomSheetStyle]}>
-                <Animated.View style={[styles.handleBar, rHandleBarStyle]} />
-            </Animated.View>
-        </GestureDetector>
-    )
-}
+    }
+)
 
 export default BottomSheet
 
@@ -81,6 +105,7 @@ const styles = StyleSheet.create({
     handleBar: {
         width: 0.175 * SCREEN_WIDTH,
         height: 5,
+        marginBottom: 22.5,
         backgroundColor: "gray",
         alignSelf: "center",
         marginVertical: 12,
